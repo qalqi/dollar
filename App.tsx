5d086795b342571799dcbbe38241f46fa7c6bc5a
cultmind <67218067+cultmind@users.noreply.github.com>
2021-09-02 16:46:58 +0530

    new branch

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome To Dollar App !</Text>
+      <Text>Welcome To Dollar App V1!</Text>
       <StatusBar style="auto" />
     </View>
   );

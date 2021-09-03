4b13d9cb77e251674b5615bec64610e893c98a01
qalqi <qalqi@qalqi.com>
2021-09-03 16:11:41 +0530

    Fix issues

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { GameEngine } from 'react-native-game-engine';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import entities from "./entities";
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <GameEngine
-      entities={entities()}
-      style={{position:'absolute', top: 0, left: 0, right: 0, bottom: 0}}
-      >
-      </GameEngine>
-      
-      <StatusBar style="auto" hidden={true}/>
+        entities={entities()}
+        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+      ></GameEngine>
+
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
-
-
--- entities/index.js
+++ entities/index.js
@@ -1,15 +1,15 @@
 import Matter from "matter-js"
-
+import Bird from '../components/bird';
 
 export default restart => {
-    let engine = Matter.Engine.create({enableSleeping: false})
+    let engine = Matter.Engine.create({ enableSleeping: false })
 
-    let world =engine.world
+    let world = engine.world
 
-    world.gravity.y= 0.4;
+    world.gravity.y = 0.4;
 
-    return{
-        physics: {engine, world},
-        Bird: Bird(world, 'green', {x: 50, y: 200}, {height: 40, width: 40})
+    return {
+        physics: { engine, world },
+        Bird: Bird(world, 'green', { x: 50, y: 200 }, { height: 40, width: 40 })
     }
 }
